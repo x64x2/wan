013796a62c0cb24e9e7c72fb508da928422264b5
somto <81354428+x64x2@users.noreply.github.com>
2025-01-05 03:15:24 +0000

    aaaaaaaaaaaaaaaaahhhhhhhhhhhhhhhhhhhhhhh

---
+++ src/assets/custom_texts.h
@@ -0,0 +1,24 @@
+const unsigned char custom_text0 [] = 
+	"THIS IS A FILLER!%";
+	
+const unsigned char * const custom_texts [] = {
+	custom_text0
+};
+
+const unsigned char dialogue_portraits [] = {
+
+};
+
+const unsigned char * const dialogue_texts [] = {
+
+};
+
+const unsigned char cutscene_0 [] = 
+	"HOLA K ASE %%"
+	"PICHI";
+
+const unsigned char * const cutscenes [] = {
+	cutscene_0
+};
+
+
